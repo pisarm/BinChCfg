dc5eda9ca6b0a22919d2f55e016a1866846bbc1c
Flemming Pedersen <flemming@pisarm.dk>
2017-03-31 22:34:55 +0200

    Initial commit

---
+++ Sources/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BinChCfg
+//
+//  Created by Flemming Pedersen on 31/03/2017.
+//  Copyright © 2017 pisarm.dk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
